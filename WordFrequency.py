3e99dc9ab7204a665814f8297ac41512b95c6f76
apaul00 <98358736+apaul00@users.noreply.github.com>
2022-02-16 17:45:19 -0600

    fixed

--- WordFrequency.py
+++ WordFrequency.py
@@ -1,7 +1,7 @@
 #Write a program that reads the contents of a text file. The program should create a dictio-nary 
 #in which the keys are the individual words found in the file and the values are the number of times
 #each word appears. For example, if the word “the” appears 128 times, the dictionary would contain an 
-#element with 'the' as the key and 128 as the value. The program should display the frequency of each word.
+#element with 'the' as the key and 128 as the value. The program should display the frequency of each word
 infile = open("AI.txt", "r")
 
 word_frequency = {}
